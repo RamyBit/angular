33f7ec34df9713550879e5e8cc3de1d8186dbfef
RamyBit <ramy.j@outlook.de>
2023-03-19 09:33:53 -0400

    test 2

---
+++ src/app/shared/qanswer.ts
@@ -0,0 +1,4 @@
+export interface Qanswer {
+    txt: string[],
+    correct: boolean
+}
---
+++ src/app/shared/question.ts
@@ -0,0 +1,10 @@
+import { Qanswer } from "./qanswer";
+
+export interface Question {
+    qid: number,
+    qtyp: string,
+    qtxt: string[],
+    qanswers: Qanswer[],
+    qcorrect: string,
+    qinfo: []
+}
