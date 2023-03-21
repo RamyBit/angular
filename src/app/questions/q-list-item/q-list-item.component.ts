d7d08758b88f18533bc7713d9af0d28a46b0b973
RamyBit <ramy.j@outlook.de>
2023-03-21 07:13:15 -0400

    Mat

--- src/app/questions/q-list-item/q-list-item.component.ts
+++ src/app/questions/q-list-item/q-list-item.component.ts
@@ -7,5 +7,31 @@ import { Question } from 'src/app/shared/question';
   styleUrls: ['./q-list-item.component.css']
 })
 export class QListItemComponent {
-@Input() question?: Question
+  @Input() question?: Question
+  qtype: any = {
+    sc: false,
+    mc: false,
+    fi: false
+  }
+
+  ngOnChanges(){
+    this.setType(this.question);
+  }
+
+  setType(q?: Question){
+    if (q?.qtyp === 'sc'){
+      this.qtype.sc = true;
+      this.qtype.mc = false;
+      this.qtype.fi = false
+    }else if (q?.qtyp === 'mc'){
+      this.qtype.sc = false;
+      this.qtype.mc = true;
+      this.qtype.fi = false;
+    }else{
+      this.qtype.fi = true;
+      this.qtype.sc = false;
+      this.qtype.mc = false;
+    }
+  }
 }
+
